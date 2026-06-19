c97197ae4d600512a29298ab47742ad3f83f755a
POOJA <poojaagowdaaa@gmail.com>
2026-06-19 20:02:26 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,22 @@
-// package BASIC PROGRAMS;
-
 public class Student {
-    
+    private String name;
+    private int id;
+    private double grade;
+    public Student(String name, int id, double grade) {
+        this.name = name;
+        this.id = id;
+        this.grade = grade;
+    }
+    public void displayInfo() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Grade: " + grade);
+        System.out.println("--------------------");
+    }
+   The main execution method
+    public static void main(String[] args) {
+        System.out.println("=== Running Student Portfolio Code ===");
+        Student student1 = new Student("Pooja", 101, 95.5);
+        student1.displayInfo();
+    }
 }
